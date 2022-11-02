54eca02dcd27e4a3eba71ebb820cf145683c75f6
Julian Martinez <julian.martinez.cs@gmail.com>
2022-11-02 23:14:54 +0000

    express-async-handler installed, video controller and routes set up

--- backend/controllers/videoController.js
+++ backend/controllers/videoController.js
@@ -1,36 +1,37 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc    Get videos
 // @route   GET /api/videos
 // @access  Private
-const getVideos = (req, res) => {
+const getVideos = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Videos" });
-};
+});
 
 // @desc    Set video
 // @route   POST /api/videos
 // @access  Private
-const setVideo = (req, res) => {
-
-    if(!req.body.text) {
-        res.status(400)
-        throw new Error('Please add a text field');
-    }
+const setVideo = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
 
   res.status(200).json({ message: "Set video" });
-};
+});
 
 // @desc    Update video
 // @route   PUT /api/videos/:id
 // @access  Private
-const updateVideo = (req, res) => {
+const updateVideo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update video ${req.params.id}` });
-};
+});
 
 // @desc    Delete video
 // @route   DELETE /api/video
 // @access  Private
-const deleteVideo = (req, res) => {
+const deleteVideo = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete video ${req.params.id}` });
-};
+});
 
 module.exports = {
   getVideos,

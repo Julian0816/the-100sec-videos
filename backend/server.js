0a609445f4b258494bf3fc172ece3733e46620ec
Julian Martinez <julian.martinez.cs@gmail.com>
2022-11-02 19:37:04 +0000

    Created express app and first route

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('sup dude?');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = 8080;
+
+const app = express();
+
+app.get('/api/videos', (req, res) => {
+    res.status(200).json({ message: 'Get Videos' })
+})
+
+
+
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));

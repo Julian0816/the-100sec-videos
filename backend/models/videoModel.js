1c3da87dc7b00b00f4806dcf472bb8062fdd1de0
Julian Martinez <julian.martinez.cs@gmail.com>
2022-11-09 13:44:37 +0000

    Created user model and added it to the videos model for referencing

--- backend/models/videoModel.js
+++ backend/models/videoModel.js
@@ -2,9 +2,28 @@ const mongoose = require("mongoose");
 
 const videoSchema = mongoose.Schema(
   {
-    name: String,
-    description: String,
-    videoAddress: String,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    }
+  },
+  {
+    // name: String,
+    // description: String,
+    // videoAddress: String,
+    name: {
+      type: String,
+      required: [true, 'Please add a name'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please add a description'],
+    },
+    videoAddress: {
+      type: String,
+      required: [true, 'Please add a video'],
+    }
   },
   {
     timestamps: true,
